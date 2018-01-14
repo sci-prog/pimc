33fa24d3d462cd63fa18ad6dd7e43b69c7dc0300
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2018-01-14 15:02:43 -0500

    Implementación imperativa

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,16 @@ extern crate rand;
 use rand::distributions::{IndependentSample, Range};
 
 fn main() {
-   let between = Range::new(-1f64, 1.);
-   let mut rng = rand::thread_rng();
-
-   let total = 1_000_000;
-   let mut in_circle = 0;
-
-   for _ in 0..total {
-       let a = between.ind_sample(&mut rng);
-       let b = between.ind_sample(&mut rng);
-       if a*a + b*b <= 1. {
-           in_circle += 1;
-       }
-   }
-
-   // prints something close to 3.14159...
-   println!("{}", 4. * (in_circle as f64) / (total as f64));
+    let between = Range::new(-1f64, 1.);
+    let mut rng = rand::thread_rng();
+    let total = 1_000_000;
+    let in_circle: i32 = (0..total)
+        .map(|_| (
+                between.ind_sample(&mut rng),
+                between.ind_sample(&mut rng)
+                ))
+        .map(|(a, b)| if a*a + b*b <= 1. { 1 } else { 0 })
+        .sum();
+    // prints something close to 3.14159...
+    println!("{}", 4. * (in_circle as f64) / (total as f64));
 }
