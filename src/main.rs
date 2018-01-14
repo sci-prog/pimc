0549b27032c3cc6b562fecba32f486599fbf7a25
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2018-01-14 12:10:58 -0500

    Implement pi estimation imperative style

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 extern crate rand;
 
+use rand::distributions::{IndependentSample, Range};
+
 fn main() {
-    println!("Hello, world!");
+   let between = Range::new(-1f64, 1.);
+   let mut rng = rand::thread_rng();
+
+   let total = 1_000_000;
+   let mut in_circle = 0;
+
+   for _ in 0..total {
+       let a = between.ind_sample(&mut rng);
+       let b = between.ind_sample(&mut rng);
+       if a*a + b*b <= 1. {
+           in_circle += 1;
+       }
+   }
+
+   // prints something close to 3.14159...
+   println!("{}", 4. * (in_circle as f64) / (total as f64));
 }
